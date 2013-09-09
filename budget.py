4414785681a25a605e4f8d0b5b3fbc0bc203b014
huangyi <yi.codeplayer@gmail.com>
2013-09-09 16:54:53 +0800

    update

--- budget.py
+++ budget.py
@@ -9,8 +9,10 @@ sizes = OrderedDict([
     (u'厨房', (2350+1800+60)*1500  / (1000*1000.0)),
     (u'阳台', (1840*3800 / (1000*1000.0))),
     (u'墙砖', ((2350+1800+60)*2800 + 3537*2800*2 + 1800*2800*2 + 1840*2800) / (1000*1000.0)),
+    (u'墙漆', 120),
 ])
 
+wall_size = (3200 * 2800) / (1000*1000.0) # 主卧次卧墙面积
 #cabinet_size = (3200 * 2800) / (1000*1000.0) # 衣柜面积
 cabinet_size = (1840 * 2800) / (1000*1000.0) # 衣柜面积
 #cabinet_price = 900
@@ -27,6 +29,12 @@ prices = OrderedDict({
     u'墙砖': (7*(1000*1000)/(300*300)),        # 普通
     u'防滑地砖': (5*(1000*1000)/(300*300)),    # 普通
     u'找平': 30,
+    u'拆墙': 45,
+    u'砌墙': 80,
+    u'墙面铺砖': 48,
+    u'厨房地面铺砖': 48,
+    u'刷漆': 12,
+    u'腻子': 30,
 })
 
 print u'面积'
@@ -36,12 +44,18 @@ print
 
 majors_totals = OrderedDict([
     #(u'装修公司', 18000),
-    (u'门', 3000+800*3),
+    (u'卫生间基础工程', 952+840+660),
+    #(u'墙面刷漆', prices[u'刷漆'] * sizes[u'墙漆']),
+    (u'墙面天花处理', (prices[u'刷漆'] + prices[u'腻子']) * sizes[u'墙漆']),
     (u'地面找平', prices[u'找平'] * (sizes[u'客厅'] + sizes[u'主卧'] + sizes[u'次卧'])),
+    (u'拆墙砌墙', wall_size * (prices[u'拆墙'] + prices[u'砌墙'])),
+    (u'门（换大门）', 3000+800*3),
     (u'客厅地板', prices[u'地板'] * sizes[u'客厅']),
     #(u'客厅地砖', 80*40),
     (u'房间地板', prices[u'地板'] * (sizes[u'主卧'] + sizes[u'次卧'])),
-    (u'防滑地砖', (sizes[u'卫生间'] + sizes[u'厨房'] + sizes[u'阳台']) * prices[u'防滑地砖']),
+    (u'厨卫阳台地砖铺设', prices[u'厨房地面铺砖'] * sizes[u'厨房']),
+    (u'厨卫阳台地砖', (sizes[u'卫生间'] + sizes[u'厨房'] + sizes[u'阳台']) * prices[u'防滑地砖']),
+    (u'墙砖铺设', prices[u'墙面铺砖'] * sizes[u'墙砖']),
     (u'墙砖', sizes[u'墙砖']*prices[u'墙砖']),
     (u'厨卫吊顶', 2180), # 欧斯宝团购方案
     (u'衣柜', cabinet_size*cabinet_price),
@@ -63,7 +77,7 @@ majors_totals = OrderedDict([
     #(u'餐桌', 2000),
 ])
 
-print u'主材总价：'
+print u'总价：'
 for k, v in majors_totals.items():
     print k, v
 print
