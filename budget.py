11bbf636ff2cf1d9fc73675f91a3743bc5037ae1
yihuang <yi.codeplayer@gmail.com>
2013-09-23 11:52:27 +0800

    update

--- budget.py
+++ budget.py
@@ -2,15 +2,32 @@
 from collections import OrderedDict
 
 sizes = OrderedDict([
-    (u'主卧', (3680*3200 + (3680/2)*500) / (1000*1000.0)),
-    (u'次卧', 2350*3200  / (1000*1000.0)),
-    (u'客厅', (5937*3554) / (1000*1000.0)),
-    (u'卫生间', 3537*1800  / (1000*1000.0)),
-    (u'厨房', (2350+1800+60)*1500  / (1000*1000.0)),
-    (u'阳台', (1840*3800 / (1000*1000.0))),
-    (u'墙砖', ((2350+1800+60)*2800 + 3537*2800*2 + 1800*2800*2 + 1840*2800) / (1000*1000.0)),
-    (u'刷漆', 139),
+    (u'主卧', (1900*2900 + 1360*3560) / (1000*1000.0)),
+    (u'次卧', 2430*2970  / (1000*1000.0)),
+    (u'客厅', (3540*5980) / (1000*1000.0)),
+    (u'卫生间', (1580*2970) / (1000*1000.0)),
+    (u'厨房', (3700*1340) / (1000*1000.0)),
+    (u'生活阳台', (1330*1340) / (1000*1000.0)),
+    (u'阳台', (1360*3540 / (1000*1000.0))),
 ])
+
+print u'面积'
+for k,v in sizes.items():
+    print k, v, u'平'
+print
+print u'合计：', sum(sizes.values()), u'平'
+print
+
+other_sizes = OrderedDict([
+    (u'墙砖', ((2970*2+1580+710)*2800 + (3700+2120+1330*2+1340+640)*2800) / (1000*1000.0)),
+    (u'刷漆', 139),
+    (u'地砖', sizes[u'阳台'] + sizes[u'生活阳台'] + sizes[u'厨房'] + sizes[u'卫生间']),
+])
+
+print u'施工面积'
+for k,v in other_sizes.items():
+    print k, v, u'平'
+print
 
 wall_size = (3200 * 2800) / (1000*1000.0) # 主卧次卧墙面积
 #cabinet_size = (3200 * 2800) / (1000*1000.0) # 衣柜面积
@@ -38,18 +55,13 @@ prices = OrderedDict({
     u'防水': 42,
 })
 
-print u'面积'
-for k,v in sizes.items():
-    print k, v, u'平'
-print
-
 basic_totals = OrderedDict([
     (u'卫生间基础工程', 3000),
-    (u'墙面天花刷漆', prices[u'刷漆'] * sizes[u'刷漆']),
+    (u'墙面天花刷漆', prices[u'刷漆'] * other_sizes[u'刷漆']),
     (u'地面找平', prices[u'找平'] * (sizes[u'客厅'] + sizes[u'主卧'] + sizes[u'次卧'])),
     (u'厨卫阳台地砖铺设', prices[u'厨房地面铺砖'] * (sizes[u'厨房'] + sizes[u'阳台'] + sizes[u'卫生间'])),
-    (u'防水', prices[u'防水'] * (sizes[u'墙砖'] + sizes[u'厨房'] + sizes[u'阳台'] + sizes[u'卫生间'])),
-    (u'墙砖铺设', prices[u'墙面铺砖'] * sizes[u'墙砖']),
+    (u'防水', prices[u'防水'] * (other_sizes[u'墙砖'] + other_sizes[u'地砖'])),
+    (u'墙砖铺设', prices[u'墙面铺砖'] * other_sizes[u'墙砖']),
     (u'水电改造', 7000),
     (u'各种安装', 2000),
     (u'拆墙砌墙', 1000),
@@ -59,11 +71,12 @@ majors_totals = OrderedDict([
     (u'防盗门', 3000),
     (u'主卧次卧门', 1500*2),
     (u'卫生间门', 1619),
-    (u'多乐士套装', 1300),
-    (u'客厅地板', prices[u'地板'] * sizes[u'客厅']),
-    (u'房间地板', prices[u'地板'] * (sizes[u'主卧'] + sizes[u'次卧'])),
+    (u'多乐士套装', 2592),
+    (u'地板', 5708.16),
+    #(u'客厅地板', prices[u'地板'] * sizes[u'客厅']),
+    #(u'房间地板', prices[u'地板'] * (sizes[u'主卧'] + sizes[u'次卧'])),
     (u'厨卫阳台地砖', (sizes[u'卫生间'] + sizes[u'厨房'] + sizes[u'阳台']) * prices[u'防滑地砖']),
-    (u'墙砖', sizes[u'墙砖']*prices[u'墙砖']),
+    (u'墙砖', other_sizes[u'墙砖']*prices[u'墙砖']),
     (u'厨卫吊顶', 2500),    # 欧斯宝团购方案
     (u'衣柜', 5100),        # 好莱客报价
     (u'橱柜', 10000),       # 皮阿诺
@@ -80,7 +93,7 @@ majors_totals = OrderedDict([
     (u'餐桌', 2000),
 ])
 
-print u'装修公司：'
+print u'人工辅料：'
 for k, v in basic_totals.items():
     print k, v
 print
